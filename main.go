71e0f674215551b383e5294ed6678612f64e0547
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-07-02 18:04:50 +0800

    add support for sge submit

--- main.go
+++ main.go
@@ -36,6 +36,11 @@ var (
 		filepath.Join(exPath, "etc", "allSteps.tsv"),
 		"pipeline",
 	)
+	mode = flag.String(
+		"mode",
+		"local",
+		"run mode:[local|sge]",
+	)
 )
 
 type Task struct {
@@ -45,13 +50,7 @@ type Task struct {
 	TaskTo   []*Task
 	ChanFrom map[string]*chan string
 	ChanTo   map[string]*chan string
-}
-
-type TaskList struct {
-	Start     map[string]*Task
-	End       map[string]*Task
-	ChanStart map[string]*chan string
-	ChanEnd   map[string]*chan string
+	Script   string
 }
 
 func main() {
@@ -60,6 +59,16 @@ func main() {
 		flag.Usage()
 		os.Exit(0)
 	}
+
+	inputInfo, _ := simple_util.File2MapArray(*input, "\t", nil)
+	os.MkdirAll(*workdir, 0755)
+	for _, item := range inputInfo {
+		sampleID := item["sampeID"]
+		os.MkdirAll(filepath.Join(*workdir, sampleID), 0755)
+	}
+	var sampleInfo = inputInfo[0]
+	var sampleID = sampleInfo["sampleID"]
+
 	cfgInfo, _ := simple_util.File2MapArray(*cfg, "\t", nil)
 
 	var taskList = make(map[string]*Task)
@@ -77,15 +86,9 @@ func main() {
 		task := Task{
 			TaskName: name,
 			TaskInfo: item,
+			Script:   filepath.Join(*workdir, sampleID, item["name"]+".sh"),
 		}
 		taskList[name] = &task
-	}
-
-	var orginTaskList = TaskList{
-		Start:     make(map[string]*Task),
-		End:       make(map[string]*Task),
-		ChanStart: make(map[string]*chan string),
-		ChanEnd:   make(map[string]*chan string),
 	}
 
 	for taskName, item := range taskList {
@@ -97,14 +100,12 @@ func main() {
 				taskList[from].TaskTo = append(taskList[from].TaskTo, item)
 				ch := make(chan string)
 				chanMap[from] = &ch
-
 			}
 		} else {
 			item.TaskFrom = append(item.TaskFrom, &startTask)
 			ch := make(chan string)
 			chanMap["Start"] = &ch
 			startTask.ChanTo[taskName] = &ch
-			orginTaskList.Start[taskName] = item
 		}
 		item.ChanFrom = chanMap
 	}
@@ -118,25 +119,38 @@ func main() {
 		}
 	}
 
+	var i = 1
 	// run
 	for taskName, item := range taskList {
-		go func(taskName string, item *Task) {
+		go func(i int, taskName string, item *Task) {
 			var froms []string
 			for _, ch := range item.ChanFrom {
 				fromInfo := <-*ch
 				froms = append(froms, fromInfo)
 			}
 			log.Printf("Run task[%s],from[%+v]", taskName, froms)
-			for _, task := range item.TaskTo {
-				*task.ChanFrom[taskName] <- taskName + "->" + task.TaskName
+			var jid = taskName
+			switch *mode {
+			case "sge":
+				oldChan := make(chan string)
+				oldChan <- strings.Join(froms, ",")
+				newChan := make(chan string)
+				simple_util.SGEsubmmit(i, []string{item.Script}, oldChan, newChan, nil)
+				jid = <-newChan
+			default:
+				simple_util.CheckErr(simple_util.RunCmd("base", item.Script))
 			}
-		}(taskName, item)
+
+			for _, task := range item.TaskTo {
+				*task.ChanFrom[taskName] <- jid
+			}
+		}(i, taskName, item)
 	}
 
 	// start goroutine
 	for taskName, ch := range startTask.ChanTo {
 		log.Printf("Start task[%s}", taskName)
-		*ch <- "start->" + taskName
+		*ch <- ""
 	}
 	// wait goroutine end
 	for taskName, ch := range endTask.ChanFrom {
