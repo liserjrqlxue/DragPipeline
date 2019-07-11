8f350701f3bdb2975c41f90ce670f7dc40cf3066
Wang Yaoshen <wangyaoshen@bgi.com>
2019-07-11 16:36:13 +0800

    use batch splitBarcode

--- script/barcode.sh
+++ script/barcode.sh
@@ -2,6 +2,7 @@
 echo $0 $@
 workdir=$1
 pipeline=$2
+list=$3
 sampleID=$3
 fq1=$4
 fq2=$5
@@ -9,18 +10,11 @@ barcode=$6
 p1=$7
 p2=$8
 
-Workdir=$workdir/$sampleID
+Workdir=$workdir/$sampleID/raw
 export PATH=$pipeline/tools:$PATH
 
 echo `date` Start splitBarcode
-echo splitBarcode \
-    -barcode $barcode \
-    -fq1 $fq1 \
-    -fq2 $fq2 \
-    -name $sampleID \
-    -p1 $p1 \
-    -p2 $p2 \
-    -outdir $Workdir/raw
-
-perl split_fq_by_indexSeq_pair.pl -o $workdir -b $barcode -s $sampleID -p1 $p1 -p2 $p2 -fq1 $fq1 -fq2 $fq2 \
+$pipeline/splitBarcode/splitBarcode \
+    -input $list \
+    -outdir $workdir \
 &&echo Done `date`
