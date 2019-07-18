4fc932abc69be7af21a69a2b01b9c526c34b118e
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-07-18 15:07:56 +0800

    add parameter for goroutine func

--- splitBarcode/splitBarcode.go
+++ splitBarcode/splitBarcode.go
@@ -292,5 +292,6 @@ func splitReads(wg2 *sync.WaitGroup, read1, read2 [4]string, pe *PE, barcodeMap 
 	FQ[0] = strings.Join(read1[:], "\n")
 	FQ[1] = strings.Join(read1[:], "\n")
 	//go func() { sample.FQ <- FQ }()
-	sample.FQ <- FQ
+	//sample.FQ <- FQ
+	go func(sample *Sample) { sample.FQ <- FQ }(sample)
 }
